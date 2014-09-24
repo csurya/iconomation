55061f6fb84c82c2e1a845d87f8dab2a1102a96f
Chrisanthy Surya <csurya@nyu.edu>
2014-09-24 15:46:21 -0400

    a

--- scripts/iconomation.js
+++ scripts/iconomation.js
@@ -32,6 +32,8 @@ $('.em-woman').animate({
 }, 1000);
 
 //after a delay, show music note
+for(i=0; i<5;i++){
+
 setTimeout(function()
 {
 $('.em-musical_note').css({
@@ -45,7 +47,7 @@ $('.em-musical_note').animate({
 }, 2000);
 }, 1000);
 
-
+}
 
 
 
